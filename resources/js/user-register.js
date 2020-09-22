e1b589f0df1978c917975c91bde06c982d58c8af
Felipe Meregildo <fmeregildo@brujula.es>
2020-09-22 12:19:25 +0200

    validate form register

--- resources/js/user-register.js
+++ resources/js/user-register.js
@@ -1,4 +1,52 @@
 $(function () {
     
-   
+
+
+   $("#frmUserRegister").validate({
+        
+        rules: {
+            txtNombre: {
+                "required": true,
+                "maxlength": 10
+            },
+            txtApellidos: {
+                "required": true,
+                "maxlength": 20
+            },
+            txtTelefono: {
+                "digits" : true
+            },
+            txtEmail: {
+                "required": true,
+                "email": true
+            }
+        },
+        messages: {
+            txtNombre:  {
+                "required": "Por favor, ingrese el nombre.",
+                "maxlength": "El nombre no puede superar los 10 caracteres."
+            },
+            txtApellidos:  {
+                "required": "Por favor, ingrese los apellidos.",
+                "maxlength": "Los apellidos no pueden superar los 20 caracteres."
+            },
+            txtTelefono: {
+                "digits" : "Ingrese un número de teléfono válido."
+            },
+            txtEmail: {
+                "required": "Por favor, ingrese el email.",
+                "email": "Ingrese un email válido."
+            }
+        },
+        submitHandler: function(form) {
+           
+            $(".alert-success").show();
+            form.reset();
+          }
+    });
+
+    $(".alert-success").hide();
+
 });
+
+
